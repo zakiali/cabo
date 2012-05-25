1e5504b33c0a1880a8674a69bb6895ddb8c34946
Zaki Ali <zakiali88@gmail.com>
2012-05-25 16:55:02 -0700

    added calfile (blo6072.py)
    moved correct_blo.. into scripts dir.

---
+++ za/calfiles/blo6072.py
@@ -0,0 +1,39 @@
+import aipy as a
+
+class AntennaArray(a.fit.AntennaArray):
+    def sim_cache(self, *args, **kwds):
+        return a.fit.AntennaArray.sim_cache(self, *args, **kwds)
+    def sim(self, i, j, pol):
+        ans = a.fit.AntennaArray.sim(self, i, j, pol)
+        return ans
+
+prms = {
+    'loc': ('+37:55.1','-122:09.4'),
+    'antpos': {
+        0: [0., 0., 0.] ,
+        #1: [16., 44.3, -10.] , 
+        1: [11.6, 44.3, -30.6] , 
+        2: [11.6, 44.3, -30.6] , 
+        3: [11.6, 44.3, -30.6] , 
+    }
+ 
+}
+
+def get_aa(freqs):
+    location = prms['loc']
+    antennas = []
+    beam = a.fit.Beam(freqs)
+    for i in range (len(prms['antpos'])):
+        pos = prms['antpos'][i]
+        antennas.append(a.fit.Antenna(pos[0], pos[1], pos[2], beam, amp=.05))
+    aa = AntennaArray(prms['loc'], antennas)
+    return aa
+
+src_prms = {
+    'Sun': {'jys':1e5},
+}
+
+def get_catalog(srcs=None, cutoff=None, catalogs=None):
+    cat = a.src.get_catalog(srcs=srcs, cutoff=cutoff, catalogs=catalogs)
+    cat.set_params(src_prms)
+ 
