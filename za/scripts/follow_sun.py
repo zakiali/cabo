19286db076efbb2f3861934775c18d920239ff84
Zaki Ali <zakiali88@gmail.com>
2012-06-06 17:43:04 -0700

    added more functionality to follow_sun.py script.
    Can track sources now if you pass it a -s <src> parameter.
    Also can just use one file to plot. 2 files is still max

--- za/scripts/follow_sun.py
+++ za/scripts/follow_sun.py
@@ -4,12 +4,13 @@ import sys, ephem, optparse
 
 o = optparse.OptionParser()
 o.set_description(__doc__)
-a.scripting.add_standard_options(o, cal=True)
+a.scripting.add_standard_options(o, cal=True, src=True)
 o.add_option('-p', dest='plot', action='store_true',
     help='plot the data in the two .npz files or not. specify the file names if option is given with the -f option. .') 
 o.add_option('-f', '--files', dest='files', type='str', 
     help='files to plot in -p option is given.file name distinguished by a _.')
 opts, args = o.parse_args(sys.argv[1:])
+
 
 print opts.plot
 if opts.plot:
@@ -21,18 +22,27 @@ deg2rad = n.pi/180.
 rad2deg = 180/n.pi
 if not opts.plot:
 
+    sources,cutoff,catalog = a.scripting.parse_srcs(opts.src, opts.cat)
+    colors = ['b','g','r','c','m','y','k','w']
+    if not ('Sun' in sources):
+        sources.append('Sun') # default source. 
     sdf = .5/1024.
 
     freqs = n.arange(1024)*sdf + (sdf + .5) # IN GHz
     #set up observer.
     aa = a.cal.get_aa(opts.cal,freqs)
-    sun = ephem.Sun()
+    cat = a.cal.get_catalog(opts.cal, sources, cutoff, catalog)
+    sun = ephem.Sun() # default source
     ad = {}
-    ad['az'] = []
-    ad['alt'] = []
-    ad['ra'] = []
-    ad['dec'] = []
-    ad['times'] = []
+    ad['sources'] = []
+    for src in sources:
+        ad[src+'az'] = []
+        ad[src+'alt'] = []
+        ad[src+'ra'] = []
+        ad[src+'dec'] = []
+        ad[src+'times'] = []
+        ad['sources'].append(src)
+
     for file in args:
         print 'processing  %s'%file
         uvi = a.miriad.UV(file)
@@ -41,47 +51,56 @@ if not opts.plot:
         for p,d,f in uvi.all(raw=True): 
             crd, t, (i,j) = p 
             aa.set_jultime(t)
-            sun.compute(aa)
-            ad['times'].append(t)
-            ad['az'].append(sun.az)
-            ad['alt'].append(sun.alt)
-            ad['ra'].append(sun.ra)
-            ad['dec'].append(sun.dec)
+            for src in sources:
+                currsrc = cat[src]
+                currsrc.compute(aa)
+                ad[src+'times'].append(t)
+                ad[src+'az'].append(currsrc.az)
+                ad[src+'alt'].append(currsrc.alt)
+                ad[src+'ra'].append(currsrc.ra)
+                ad[src+'dec'].append(currsrc.dec)
     #       print sun.ra, sun.dec 
 
-    for key in ad.keys():
-        ad[key] = n.array(ad[key])
+    for s in cat.keys():
+        for key in ['az','alt','ra','dec','times']:
+            ad[s+key] = n.array(ad[s+key])
 
-    print 'Saving data to ->',str(ad['times'][0])+'.npz'
-    n.savez(str(ad['times'][0])+'.npz', **ad)
+    print 'Saving data to ->',str(ad[src+'times'][0])+'.npz'
+    n.savez(str(ad[src+'times'][0])+'.npz', **ad)
 
 if opts.plot:
-    if opts.files.find('_')i != -1:
+    if opts.files.find('_') != -1:
         f1,f2 = opts.files.split('_')
         d1 = n.load(f1) 
         d2 = n.load(f2) 
-        plt.subplot(211)
-        plt.title('az vs. alt')
-        plt.xlabel('az')
-        plt.ylabel('alt')
-        plt.plot(d1['az']*rad2deg,d1['alt']*rad2deg,'.b',d2['az']*rad2deg,d2['alt']*rad2deg,'.r')
-        plt.subplot(212)
-        plt.title('ra vs. dec')
-        plt.xlabel('ra')
-        plt.ylabel('dec')
-        plt.plot(d1['ra'],d1['dec'],'.b',d2['ra'],d2['dec'],'.r')
-        plt.show()
+        for num,src in enumerate(d1['sources']):
+            print src
+            fig = plt.figure(num)
+            fig.text(0.5,0.975,'%s'%src)         
+            plt.subplot(211)
+            plt.title('az vs. alt')
+            plt.xlabel('az')
+            plt.ylabel('alt')
+            plt.plot(d1[src+'az']*rad2deg,d1[src+'alt']*rad2deg,'.b',d2[src+'az']*rad2deg,d2[src+'alt']*rad2deg,'.r')
+            plt.subplot(212)
+            plt.title('ra vs. dec')
+            plt.xlabel('ra')
+            plt.ylabel('dec')
+            plt.plot(d1[src+'ra'],d1[src+'dec'],'.b',d2[src+'ra'],d2[src+'dec'],'.r')
     else: 
         f = opts.files
         d = n.load(f)
-        plt.subplot(211)
-        plt.title('az vs. alt')
-        plt.xlabel('az')
-        plt.ylabel('alt')
-        plt.plot(d['az']*rad2deg,d['alt']*rad2deg,'.b')
-        plt.subplot(212)
-        plt.title('ra vs. dec')
-        plt.xlabel('ra')
-        plt.ylabel('dec')
-        plt.plot(d['ra'],d['dec'],'.b')
-        plt.show()
+        for num,src in enumerate(d['sources']):
+            fig = plt.figure(num)
+            fig.text(0.5,0.975,'%s'%src)         
+            plt.subplot(211)
+            plt.title('az vs. alt')
+            plt.xlabel('az')
+            plt.ylabel('alt')
+            plt.plot(d[src+'az']*rad2deg,d[src+'alt']*rad2deg,'.b')
+            plt.subplot(212)
+            plt.title('ra vs. dec')
+            plt.xlabel('ra')
+            plt.ylabel('dec')
+            plt.plot(d[src+'ra'],d[src+'dec'],'.b')
+    plt.show()
